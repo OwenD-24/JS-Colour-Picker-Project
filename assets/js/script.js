87d63d290ebe05d9b6706422330f2820e1ef240d
Owen Davis <owendavis1991@gmail.com>
2024-08-20 18:22:25 +0000

    Add Click Event Listeners to Copy Colours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 // Code for Selecting Elements and Setting up Event Listeners
 const colorCards = document.querySelectorAll('.color-cards');
 const btnEl = document.getElementById('button');
+
+// Code for Click Event Listeners to Copy Colours
+colorCards.forEach(card => {
+    card.addEventListener('click', () => {
+        copyClipboard(card.querySelector('p').innerHTML);
+    });
+});
