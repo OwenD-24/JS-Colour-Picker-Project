8fe5b22b6f46fd5badfe12f5fddb26ec60457135
Owen Davis <owendavis1991@gmail.com>
2024-08-20 20:18:57 +0000

    Add code for Generating the random colours

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,17 @@ function createPalette() {
         card.querySelector('p').innerText = newColor;
     });
 }
+
+// Generating a Random Colour
+function generatedColor() { 
+    const hexArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+        'a', 'b', 'c', 'd', 'e', 'g']; 
+
+    let color = '#';
+
+    for (let i = 0; i < 7; i++) { 
+        color += hexArray[Math.floor(Math.random() * hexArray.length)];
+    }
+
+    return color;
+}
