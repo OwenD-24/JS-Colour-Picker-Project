ca7fa02037e2d8c6ad9c4e2fb5cc5811ee51d9de
Owen Davis <owendavis1991@gmail.com>
2024-08-20 18:39:45 +0000

    Corrected code for setting up event listeners, had incorrect color-card class and incorrect btn ID

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Code for Selecting Elements and Setting up Event Listeners
-const colorCards = document.querySelectorAll('.color-cards');
-const btnEl = document.getElementById('button');
+const colorCards = document.querySelectorAll('.color-card');
+const btnEl = document.getElementById('btn');
 
 // Code for Click Event Listeners to Copy Colours
 colorCards.forEach(card => {
