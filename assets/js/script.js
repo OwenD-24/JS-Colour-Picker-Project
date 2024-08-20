51f57611790493809e23b6012ad3622fe3683dd8
Owen Davis <owendavis1991@gmail.com>
2024-08-20 20:29:31 +0000

    Corrected hexArray and added an 'f' array

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ function createPalette() {
 // Generating a Random Colour
 function generateColor() { 
     const hexArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-        'a', 'b', 'c', 'd', 'e']; 
+        'a', 'b', 'c', 'd', 'e', 'f']; 
 
     let color = '#';
 
