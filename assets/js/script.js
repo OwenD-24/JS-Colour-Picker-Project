ad0166adec1500062b966e97f40f9771b94d1a51
Owen Davis <owendavis1991@gmail.com>
2024-08-20 20:23:41 +0000

    Corrected incorrect typos for function name, had too many letters in hex array and had an incorrect loop count

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,13 +49,13 @@ function createPalette() {
 }
 
 // Generating a Random Colour
-function generatedColor() { 
+function generateColor() { 
     const hexArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-        'a', 'b', 'c', 'd', 'e', 'g']; 
+        'a', 'b', 'c', 'd', 'e']; 
 
     let color = '#';
 
-    for (let i = 0; i < 7; i++) { 
+    for (let i = 0; i < 6; i++) { 
         color += hexArray[Math.floor(Math.random() * hexArray.length)];
     }
 
